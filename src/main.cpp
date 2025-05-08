3061fc554411fc395f25e09f068fadd689fafec8
Aryadeep Pradhan <aryadeep.pradhan@gmail.com>
2025-05-08 17:38:34 +0530

    handled invalid commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-
+using namespace std;
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  cout << unitbuf;
+  cerr << unitbuf;//similar to cout but specifically used for printing error message. Used when you want to log error
 
   // Uncomment this block to pass the first stage
-  std::cout << "$ ";
+  cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
+  string input;
+  getline(cin, input);
+  cout<< input<<":command not found";
 }
