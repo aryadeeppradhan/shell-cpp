1be4982af3b981222f9fee906f2755aa2d160c17
Aryadeep Pradhan <aryadeep.pradhan@gmail.com>
2025-05-09 16:34:39 +0530

    added echo builtin

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 using namespace std;
 int main() {
-  // Flush after every std::cout / std:cerr
   cout << unitbuf;
   cerr << unitbuf;//similar to cout but specifically used for printing error message. Used when you want to log error
-
-  // Uncomment this block to pass the first stage
   cout << "$ ";
 
   string input;
   while(getline(cin, input)){
     if(input=="exit 0"){
       return 0;
-    }
+    }else if(input.find("echo")!=string::npos){
+      cout<<input.substr(5)<<endl;
+      cout<<"$ ";
+    }else{
   cout<< input<<": command not found"<<endl;
   cout<<"$ ";
+    }
   }
 }
