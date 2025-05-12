3c2275f1198583d167affdd6329b6a2e7c6bb49c
Aryadeep Pradhan <aryadeep.pradhan@gmail.com>
2025-05-12 23:12:58 +0530

    removed path code

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
-#include <unistd.h>
-#include <string>
-#include <cstdlib>
-#include <filesystem>
-#include <sstream>
 using namespace std;
-namespace fs = std::filesystem;
 int main() {
   cout << unitbuf;
   cerr << unitbuf;//similar to cout but specifically used for printing error message. Used when you want to log error
@@ -25,26 +19,10 @@ int main() {
       if(a=="echo"||a=="exit"||a=="type"){
         cout<<a+" is a shell builtin"<<endl;
  
-      }else {
-        // Get PATH from environment
-        const char* path_env = getenv("PATH");
-        if (path_env) {
-          stringstream ss(path_env);
-          string dir;
-          bool found = false;
-          while (getline(ss, dir, ':')) {
-            fs::path full_path = fs::path(dir) / a;
-            if (fs::exists(full_path) && fs::is_regular_file(full_path) && access(full_path.c_str(), X_OK) == 0) {
-              cout << a << " is " << full_path << endl;
-              found = true;
-              break;
-            }
-          } if (!found) {
-            cout << a << ": not found" << endl;
-          }
-        }else {
-          cout << "PATH environment variable not set." << endl;
-        }
+      }
+      else{
+        cout<<a+": not found"<<endl;
+
       }
       }else{
   cout<<input+": command not found"<<endl;
