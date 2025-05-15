1ca670d57ed42eebfb52ffea6bf58a7b5a5313f6
Aryadeep Pradhan <aryadeep.pradhan@gmail.com>
2025-05-15 22:37:57 +0530

    added code for external program

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
-#include<unistd.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include<filesystem>
 using namespace std;
 namespace fs = filesystem;
 int main() {
@@ -31,7 +33,7 @@ int main() {
                     bool found = false;
 
                     // Step 3: Split by ':' and check each dir
-                    while (getline(ss, dir, ':')) {
+                    while (getline(ss, dir, ':')) {//jab tak ':' nahi milta tab tak line ko read karta rahega
                         string fullPath = dir + "/" + a;
                         if (fs::exists(fullPath) && fs::is_regular_file(fullPath) && access(fullPath.c_str(), X_OK) == 0) {
                             cout << a << " is " << fullPath << endl;
@@ -45,8 +47,55 @@ int main() {
                 }
             }
         }else{
-  cout<<input+": command not found"<<endl;
-    }
+          //handling external commands
+          stringstream iss(input);
+          vector<string> tokens;
+          string token;
+          while(iss>>token){//ek ek word jo stringstream toda wo token me daal dega
+            tokens.push_back(token);
+          }
+          if(tokens.empty()){
+            cout<<"$ ";
+            continue;
+          }
+           string command = tokens[0];
+            const char* pathEnv = getenv("PATH");
+            bool found = false;
+            string fullPath;
+
+            if (pathEnv != nullptr) {
+                stringstream ss(pathEnv);
+                string dir;
+                while (getline(ss, dir, ':')) {
+                    string tempPath = dir + "/" + command;
+                    if (fs::exists(tempPath) && fs::is_regular_file(tempPath) && access(tempPath.c_str(), X_OK) == 0) {
+                        fullPath = tempPath;
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!found) {
+                cout << command << ": command not found" << endl;
+            }else{
+              //building argv
+              vector<char*> argv;
+              for(const string& arg : tokens) {
+                argv.push_back(const_cast<char*>(arg.c_str()));//c_str() returns a const char* pointer to the string's data. Par execv ko const char* nahi chahiye. essiliye const_cast use kiya taki wo const char* ko char* me convert kar sake
+              }
+              argv.push_back(nullptr);//execv ko last me null pointer chahiye hota hai
+              pid_t pid = fork();  // create child
+              if(pid == 0){
+                execv(fullPath.c_str(), argv.data());
+                perror("execv");
+                exit(1);
+              } else {
+                int status;
+                waitpid(pid, &status, 0);
+                }
+            }
+    } 
     cout<<"$ ";
   }
   return 0;
